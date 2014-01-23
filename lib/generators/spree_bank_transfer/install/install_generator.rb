6eae7ab5efc05efec1862e7dffff925bae1da3bc
mohitbansal <mohit.bansal@vinsol.com>
2014-01-23 17:19:53 +0530

    Update install_generator.rb

--- lib/generators/spree_bank_transfer/install/install_generator.rb
+++ lib/generators/spree_bank_transfer/install/install_generator.rb
@@ -5,8 +5,8 @@ module SpreeBankTransfer
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_bank_transfer\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_bank_transfer\n"
+        append_file 'app/assets/javascripts/store/all.js', "\n//= require store/spree_bank_transfer\n"
+        append_file 'app/assets/javascripts/admin/all.js', "\n//= require admin/spree_bank_transfer\n"
       end
 
       def add_stylesheets
