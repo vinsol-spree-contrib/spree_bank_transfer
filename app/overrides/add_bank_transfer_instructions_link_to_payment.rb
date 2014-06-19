876cf20bccd883c12f2c5b268e116d7691b8e6d3
mohit <mohit.bansal@vinsol.com>
2014-06-19 11:16:12 +0530

    show how bank transfer works link only when bank transfer payment method present

--- app/overrides/add_bank_transfer_instructions_link_to_payment.rb
+++ app/overrides/add_bank_transfer_instructions_link_to_payment.rb
@@ -3,7 +3,9 @@ Deface::Override.new(
   :name => 'add_bank_transfer_instructions_link_to_payment',
   :insert_after => "#payment-method-fields",
   :text => %q{
-    <p><%= link_to "How Bank Transfer works?", bank_transfer_instructions_path, :id => 'banktransfer_instructions', :target => "_blank" %></p>
+    <% if @order.available_payment_methods.any? { |pm| pm.type == 'Spree::PaymentMethod::BankTransfer' } %>
+      <p><%= link_to "How Bank Transfer works?", bank_transfer_instructions_path, :id => 'banktransfer_instructions', :target => "_blank" %></p>
+    <% end %>
   }
 )
 
