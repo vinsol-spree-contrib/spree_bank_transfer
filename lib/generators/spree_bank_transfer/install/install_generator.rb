48b4a64fca6649b60680cea40d193579d7d53f2b
redglory <pedropinto.inbox@gmail.com>
2015-06-26 00:56:32 +0100

    changed assets directory references

--- lib/generators/spree_bank_transfer/install/install_generator.rb
+++ lib/generators/spree_bank_transfer/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeBankTransfer
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require store/spree_bank_transfer\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "\n//= require admin/spree_bank_transfer\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require spree/frontend/spree_bank_transfer\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "\n//= require spree/backend/spree_bank_transfer\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require store/spree_bank_transfer\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require admin/spree_bank_transfer\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_bank_transfer\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_bank_transfer\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
